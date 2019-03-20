632c5e0ad7954a744375bf728fdb5b049a120507
Mr. McDonald <private>
2019-03-19 21:30:43 -0400

    setup load hatch front/back

--- src/main/java/frc/robot/commands/LoadHatch.java
+++ src/main/java/frc/robot/commands/LoadHatch.java
@@ -22,28 +22,39 @@ public class LoadHatch extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    Robot.claw.stopHatch();
+    if(Robot.arm.armInPosition() == ArmPosition.FLOOR_FRONT){
+      Robot.arm.frontCargoShipTop();
+      Robot.claw.intakeHatch(1.0);
+      Robot.claw.closeClawTop();
+    } else{
+        Robot.arm.backCargoShipTop();
+        Robot.claw.intakeHatch(1.0);
+        Robot.claw.closeClawBottom();
+    }
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.arm.frontCargoShipTop();
-    Robot.claw.intakeHatch(1.0);
-    Robot.claw.closeClawTop();
-
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    //System.err.printf("LoadHatch %b %b\n", Robot.m_oi.isLoadHatchReleased(), (Robot.arm.armInPosition() == ArmPosition.CARGO_SHIP_FRONT) );
-    return (Robot.m_oi.isLoadHatchReleased() || Robot.arm.armInPosition() == ArmPosition.CARGO_SHIP_FRONT);
+    if(Robot.m_oi.isLoadHatchReleased() 
+          || Robot.arm.armInPosition() == ArmPosition.CARGO_SHIP_FRONT 
+          || Robot.arm.armInPosition() == ArmPosition.CARGO_SHIP_BACK){
+      return true;
+    }
+    return false;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
     Robot.claw.stopHatch();
+    Robot.claw.closeClawTop(); 
     Robot.claw.closeClawBottom(); 
     Robot.claw.holdHatch();
   }
